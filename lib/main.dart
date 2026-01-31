e3c8ccc2aa18d137f2eba6a3ca1f2c8de4940a8b
lmaoborghini1 <kemo2901@hotmail.com>
2026-01-31 19:20:18 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'camera_screen.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(const GoToGymApp());
@@ -10,42 +10,10 @@ class GoToGymApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'GoToGym',
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('GoToGym'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-  final imagePath = await Navigator.push(
-    context,
-    MaterialPageRoute(
-      builder: (context) => const CameraScreen(),
-    ),
-  );
-
-  if (imagePath != null) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Status: DA')),
-    );
-  }
-},
-
-          child: const Text('CLICK HERE'),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
